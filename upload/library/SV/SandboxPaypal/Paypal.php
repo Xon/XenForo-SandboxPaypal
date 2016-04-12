5e440e764fed54e70b9b5c3906885fee63c5a39d
Xon <tazipper1@hotmail.com>
2016-04-13 00:10:12 +0800

    Sanity check that sandbox paypal is talking to us.

--- upload/library/SV/SandboxPaypal/Paypal.php
+++ upload/library/SV/SandboxPaypal/Paypal.php
@@ -11,7 +11,12 @@ class SV_SandboxPaypal_Paypal extends XenForo_UserUpgradeProcessor_PayPal
 			return false;
         }
 
-        $this->_filtered['test_ipn']  = true;
+        if (empty($this->_filtered['test_ipn']))
+        {
+			$errorString = new XenForo_Phrase('sandboxing_paypal_but_live_paypal_in_use');
+			XenForo_Error::logException(new Exception($errorString), false);
+			return false;
+        }
 
         return parent::validateRequest($errorString);
     }
